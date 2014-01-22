2da942cc129f6aa3da5063f4663fe3d8d29f6303
Tridion.Zen <Tridion-Zen@users.noreply.github.com>
2014-01-22 12:01:49 +0100

    Init commit

---
+++ PowerShell.DTAP.Logging.ps1
@@ -0,0 +1,34 @@
+# PowerShell.DTAP.Logging.ps1
+#
+$ErrorActionPreference = "SilentlyContinue"
+cls
+#
+$servers = @("SERVEUR_UNE", "SERVEUR_DEUX")
+#
+$startTime = get-date
+write-host "Started $($startTime)"
+#
+foreach ($server in $servers) {   
+    write-host "Server $server"
+    #
+    write-host "Searching in \XXX for logback.xml"
+    $files = get-childitem -path "\\$server\c$\XXX\" -recurse -force -include "logback.xml"
+    write-host "Files $files" -foregroundcolor gray
+    foreach ($file in $files) {
+        write-host "File $file" -foregroundcolor yellow
+        [xml] $log = get-content $file
+        $log.configuration.property
+        $log.configuration.root.level
+    } 
+    #
+    write-host "Searching in \YYY for logback.xml"
+    $files = get-childitem -path "\\$server\c$\YYY\" -recurse -force -include "logback.xml"
+    write-host "Files $files" -foregroundcolor gray
+    foreach ($file in $files) {
+        write-host "File $file" -foregroundcolor yellow
+        [xml] $log = get-content $file
+        $log.configuration.property
+        $log.configuration.root.level
+    }
+}
+write-host "Finished $(get-date)"
